7c2d5c9621abc9ad4cff9d2ff1cbcedf86fa6f3f
hzed <hzedek59@gmail.com>
2023-12-05 11:57:47 +0100

    Cards DONE

--- P07_PetitsPlats2.0/scripts/index.js
+++ P07_PetitsPlats2.0/scripts/index.js
@@ -7,6 +7,7 @@ for (let index = 0; index < recipes.length; index++) {
 
     //création du DOM Card
     const img = document.createElement("img");
+    const p_duree = document.createElement('p');
     const p_titre = document.createElement('p');
     const p_description = document.createElement('p');
     const p_recette = document.createElement('p');
@@ -14,32 +15,35 @@ for (let index = 0; index < recipes.length; index++) {
     const div = document.createElement('div')
 
     img.setAttribute('src',portrait);
-    img.classList.add('imgCard')
-    p_titre.textContent = recipes[index].name
-    p_titre.classList.add('anton')
-    p_recette.textContent = "RECETTE"
-    p_recette.classList.add('grey')
-    p_description.textContent = recipes[index].description
-    p_description.classList.add('cardText')
-    p_ingredients.textContent = "INGRÉDIENTS"
-    p_ingredients.classList.add('grey')
-    div.classList.add('ingredients')
+    img.classList.add('imgCard');
+    p_duree.textContent = card.time+"min"; 
+    p_duree.classList.add('duree');
+    p_titre.textContent = card.name;
+    p_titre.classList.add('anton','cardTextMargin','TitleSize');
+    p_recette.textContent = "RECETTE";
+    p_recette.classList.add('grey','cardTextMargin','subTitlesize');
+    p_description.textContent = card.description;
+    p_description.classList.add('cardTextMargin','cardText');
+    p_ingredients.textContent = "INGRÉDIENTS";
+    p_ingredients.classList.add('grey','cardTextMargin','subTitlesize');
+    div.classList.add('ingredients','cardTextMargin');
 
     cardContent.appendChild(article);
-    article.appendChild(img)
-    article.appendChild(p_titre)
-    article.appendChild(p_recette)
-    article.appendChild(p_description)
-    article.appendChild(p_ingredients)
-    article.appendChild(div)
+    article.appendChild(img);
+    article.appendChild(p_duree);
+    article.appendChild(p_titre);
+    article.appendChild(p_recette);
+    article.appendChild(p_description);
+    article.appendChild(p_ingredients);
+    article.appendChild(div);
 
 
     for (let i = 0; i < card.ingredients.length; i++) {
         const ingredient = card.ingredients[i];
-        const p_ingredient = document.createElement("p")
-        p_ingredient.classList.add('ingredient','cardText')
-        const p_quantity = document.createElement("p")
-        p_quantity.classList.add('cardText','grey')
+        const p_ingredient = document.createElement("p");
+        p_ingredient.classList.add('ingredient');
+        const p_quantity = document.createElement("p");
+        p_quantity.classList.add('grey');
 
         p_ingredient.textContent = ingredient.ingredient
         if(ingredient.unit){
@@ -48,8 +52,8 @@ for (let index = 0; index < recipes.length; index++) {
         else{
         p_quantity.textContent = ingredient.quantity
         }
-        div.appendChild(p_ingredient)
-        p_ingredient.appendChild(p_quantity)
+        div.appendChild(p_ingredient);
+        p_ingredient.appendChild(p_quantity);
     }
 }
 console.log(recipes);
