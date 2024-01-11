5ef9c8c32e04cd809cc0aff248ac5c40fa3ef61a
hzed <hzedek59@gmail.com>
2024-01-11 13:55:36 +0100

    li update

--- P07_PetitsPlats2.0/scripts/sort.js
+++ P07_PetitsPlats2.0/scripts/sort.js
@@ -3,77 +3,102 @@ const AppareilsBtn = document.getElementById("Appareils");
 const UstensilesBtn = document.getElementById("Ustensiles");
 const ingredientsIcon = document.getElementById("ingredientsIcon");
 const searchDOM = document.getElementById("search");
+const searchUstensiles = document.getElementById("searchUstensiles");
+const searchAppareils = document.getElementById("searchAppareils");
+const searchIngredients = document.getElementById("searchIngredients");
+const ingredientLi = document.getElementById("ingredientsLi");
+const UstensileLi = document.getElementById("UstensilesLi");
+const AppareilLi = document.getElementById("AppareilsLi");
 
 //RÉCUPÉRATION DES DONNÉES PAR BOUTTONS ET CRITÈRES
-function createLi(array) {
-  ingredientsBtn.innerHTML=`<input
-  id="searchIngredients"
-  type="text"
-  name="search"
-  class="smallSearch"
-/>
-<i class="fa-solid fa-magnifying-glass"></i>`;
-  UstensilesBtn.innerHTML=`<input
-  id="searchUstensiles"
-  type="text"
-  name="search"
-  class="smallSearch"
-/>
-<i class="fa-solid fa-magnifying-glass"></i>`;
-  AppareilsBtn.innerHTML=`<input
-  id="searchAppareils"
-  type="text"
-  name="search"
-  class="smallSearch"
-/>
-<i class="fa-solid fa-magnifying-glass"></i>`;
 
-  let ingredientsLi = [];
-for (let i = 0; i < array.length; i++) {
-  let ingredients = array[i].ingredients;
+function createLi(ul,array) {
+  let divLi = ul.children[2]; 
+  divLi.innerHTML = "";
+  let listLi = [];
+  for (let i = 0; i < array.length; i++) {
+    if (AppareilsBtn) {
+      itemLi = array[i].appliance;
+    }
+    if (UstensilesBtn) {
+      let Ustensiles = array[i].ustensils;
+    for (let x = 0; x < Ustensiles.length; x++) {
+      itemLi = Ustensiles[x];
+    }
+    }
+    if (ingredientsBtn) {
+      let ingredients = array[i].ingredients;
   for (let x = 0; x < ingredients.length; x++) {
-    let ingredient = ingredients[x].ingredient;
-    ingredientsLi.push(ingredient);
+     itemLi = ingredients[x].ingredient;
+  }
+    }
+    listLi.push(itemLi);
+  }
+  let newlistLi = [...new Set(listLi)];
+  for (let index = 0; index < newlistLi.length; index++) {
+    const li = newlistLi[index];
+    let li_dom = document.createElement("li");
+    li_dom.textContent = li;
+    divLi.appendChild(li_dom);
+  }
+  return newlistLi,
+  changeLi()
+}
+
+searchIngredients.addEventListener("input", () => trying(ingredientsBtn));
+searchUstensiles.addEventListener("input", () => searchLi(UstensilesBtn));
+searchAppareils.addEventListener("input", () => searchLi(AppareilsBtn));
+
+function trying(ul) {
+  let resultatArray = searchCards()
+  let sortedArray = sorting(resultatArray);
+
+  console.log(sortedArray);
+
+  let newLi = createLi(ul,sortedArray)
+ 
+  console.log(newLi);
+
+  let searchTermi = ul.children[0].value.toLowerCase();
+  let LiArray = newLi.filter((value) =>
+  value.toLowerCase().split(' ').some(part => part.toLowerCase().includes(searchTermi.toLowerCase()))  );
+  
+  let newLiArray = [...new Set(LiArray)];
+
+  console.log(newLiArray);
+
+  ul.children[2].innerHTML = "";
+
+  for (let i = 0; i < newLiArray.length; i++) {
+    const li = newLiArray[i];
+    let li_dom = document.createElement("li");
+    li_dom.textContent = li;
+    li_dom.classList.add("ingredientsLi");
+    ul.children[2].appendChild(li_dom);
+  }
+  changeLi()
+}
+
+function searchLi(ul) {
+  let allLi = ul.children[2].children;
+  let searchLiArray = [];
+  for (let index = 0; index < allLi.length; index++) {
+    const el = allLi[index];
+    searchLiArray.push(el);
+  }
+
+  let searchTerm = ul.children[0].value;
+  let newLiArray = searchLiArray.filter((value) =>
+    value.innerHTML.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  let liInjection = ul.children[2];
+  liInjection.innerHTML = "";
+  for (let i = 0; i < newLiArray.length; i++) {
+    const li = newLiArray[i];
+    liInjection.appendChild(li);
   }
 }
-let newIngredientsLi = [...new Set(ingredientsLi)];
-for (let index = 0; index < newIngredientsLi.length; index++) {
-  const li = newIngredientsLi[index];
-  let li_dom = document.createElement("li");
-  li_dom.textContent = li;
-  ingredientsBtn.appendChild(li_dom);
-}
-
-let AppareilsLi = [];
-for (let i = 0; i < array.length; i++) {
-  let Appareils = array[i].appliance;
-  AppareilsLi.push(Appareils);
-}
-let newAppareilsLi = [...new Set(AppareilsLi)];
-for (let index = 0; index < newAppareilsLi.length; index++) {
-  const li = newAppareilsLi[index];
-  let li_dom = document.createElement("li");
-  li_dom.textContent = li;
-  AppareilsBtn.appendChild(li_dom);
-}
-
-const UstensilesLi = [];
-for (let i = 0; i < array.length; i++) {
-  let Ustensiles = array[i].ustensils;
-  for (let x = 0; x < Ustensiles.length; x++) {
-    let Ustensile = Ustensiles[x];
-    UstensilesLi.push(Ustensile);
-  }
-}
-let newUstensilesLi = [...new Set(UstensilesLi)];
-for (let index = 0; index < newUstensilesLi.length; index++) {
-  const li = newUstensilesLi[index];
-  let li_dom = document.createElement("li");
-  li_dom.textContent = li;
-  UstensilesBtn.appendChild(li_dom);
-}
-}
-
 
 function btnToggle(btn) {
   btn.style.display = btn.style.display === "none" ? "block" : "none";
@@ -87,16 +112,6 @@ function btnToggle(btn) {
     icon.classList.add("fa-chevron-down");
   }
 }
-
-//<li> CHANGEMENT DE STYLE AU CLICK
-let li = document.querySelectorAll("li");
-li.forEach((element) => {
-  element.addEventListener("click", () => {
-    element.style.backgroundColor =
-      element.style.backgroundColor === "" ? "#ffd15b" : "";
-    element.style.margin = element.style.margin === "" ? "5px auto" : "";
-  });
-});
 
 //NBR DE RECETTES
 function nbrRecette(array) {
@@ -161,6 +176,7 @@ function searchCards() {
     let sortedArray = sorting(resultatArray);
     cardContent.innerHTML = "";
     init(sortedArray);
+    return resultatArray
   } else (cardContent.innerHTML = ""), init([...recipes]);
 }
 
@@ -169,8 +185,23 @@ searchDOM.addEventListener("input", () => searchCards());
 //Fonction qui lance l'affichage de la page
 
 function init(array) {
-  createLi(array)
+  createLi(ingredientsBtn,array);
+  createLi(AppareilsBtn,array)
+  createLi(UstensilesBtn,array)
   nbrRecette(array);
   cardDisplay(array);
 }
 init([...recipes]);
+
+//<li> CHANGEMENT DE STYLE AU CLICK
+function changeLi() {
+  let li = document.querySelectorAll("li");
+li.forEach((element) => {
+  element.addEventListener("click", () => {
+    element.style.backgroundColor =
+      element.style.backgroundColor === "" ? "#ffd15b" : "";
+    element.style.margin = element.style.margin === "" ? "5px 0px" : "";
+  });
+});
+}
+
