b78fe113f1fd68b6b9a35349197fbdc0d49246f9
hzed <hzedek59@gmail.com>
2024-02-01 12:17:32 +0100

    Implementation de la methode for each

--- P07_PetitsPlats2.0/scripts/sort.js
+++ P07_PetitsPlats2.0/scripts/sort.js
@@ -5,50 +5,84 @@ const ingredientsIcon = document.getElementById("ingredientsIcon");
 const searchDOM = document.getElementById("search");
 
 //RÉCUPÉRATION DES DONNÉES PAR BOUTTONS ET CRITÈRES
+searchDOM.addEventListener("input",()=>searchAlternative());
+function searchAlternative() {
+  if (searchDOM.value.length > 2) {
+    let searchTerm = searchDOM.value;
 
-let ingredientsLi = [];
-for (let i = 0; i < recipes.length; i++) {
-  let ingredients = recipes[i].ingredients;
-  for (let x = 0; x < ingredients.length; x++) {
-    let ingredient = ingredients[x].ingredient;
-    ingredientsLi.push(ingredient);
+    // Utilisez la méthode forEach pour créer un nouvel array avec les résultats de la recherche
+    let resultatArray = [];
+    recipes.forEach((recip) => {
+      if (recip.name.toLowerCase().includes(searchTerm.toLowerCase())) {
+        resultatArray.push(recip);
+      }
+    
+      if (recip.description.toLowerCase().includes(searchTerm.toLowerCase())) {
+        resultatArray.push(recip);
+      }
+    
+      recip.ingredients.forEach((ingredient) => {
+        if (ingredient.ingredient.toLowerCase().includes(searchTerm.toLowerCase())) {
+          resultatArray.push(recip);
+        }
+      });
+    });
+
+    let sortedArray = sorting(resultatArray);
+    init(sortedArray);
+    if (sortedArray.length == 0) {
+      cardContent.innerHTML = `<p>Aucune recette ne contient "${searchTerm}"vous pouvez chercher «tarte aux pommes », « poisson », etc..</p>`;
+    }
+    return sortedArray;
+    
+  } else init([...recipes]);
+}
+function sorting(array) {
+  const uniqueElements = {};
+  const sortedArray = array.filter((result) => {
+    // Si l'élément n'est pas déjà présent, l'ajouter à l'objet et inclure l'élément
+    if (!uniqueElements[result.id]) {
+      uniqueElements[result.id] = true;
+      return true;
+    }
+    // Sinon, l'élément est un doublon, ne pas l'inclure
+    return false;
+  });
+  return sortedArray;
+}
+function createLi(ul, array) {
+  let idLi = ul;
+  let divLi = ul.children[2];
+  divLi.innerHTML = "";
+  let listLi = [];
+  for (let i = 0; i < array.length; i++) {
+    if (idLi === AppareilsBtn) {
+      itemLi = array[i].appliance;
+      listLi.push(itemLi.toLowerCase());
+    }
+    if (UstensilesBtn === idLi) {
+      let Ustensiles = array[i].ustensils;
+      for (let x = 0; x < Ustensiles.length; x++) {
+        itemLi = Ustensiles[x];
+        listLi.push(itemLi.toLowerCase());
+      }
+    }
+    if (ingredientsBtn === idLi) {
+      let ingredients = array[i].ingredients;
+      for (let x = 0; x < ingredients.length; x++) {
+        itemLi = ingredients[x].ingredient;
+        listLi.push(itemLi.toLowerCase());
+      }
+    }
   }
-}
-let newIngredientsLi = [...new Set(ingredientsLi)];
-for (let index = 0; index < newIngredientsLi.length; index++) {
-  const li = newIngredientsLi[index];
-  let li_dom = document.createElement("li");
-  li_dom.textContent = li;
-  ingredientsBtn.appendChild(li_dom);
-}
-
-let AppareilsLi = [];
-for (let i = 0; i < recipes.length; i++) {
-  let Appareils = recipes[i].appliance;
-  AppareilsLi.push(Appareils);
-}
-let newAppareilsLi = [...new Set(AppareilsLi)];
-for (let index = 0; index < newAppareilsLi.length; index++) {
-  const li = newAppareilsLi[index];
-  let li_dom = document.createElement("li");
-  li_dom.textContent = li;
-  AppareilsBtn.appendChild(li_dom);
-}
-
-const UstensilesLi = [];
-for (let i = 0; i < recipes.length; i++) {
-  let Ustensiles = recipes[i].ustensils;
-  for (let x = 0; x < Ustensiles.length; x++) {
-    let Ustensile = Ustensiles[x];
-    UstensilesLi.push(Ustensile);
+  let newlistLi = [...new Set(listLi)];
+  for (let index = 0; index < newlistLi.length; index++) {
+    const li = newlistLi[index];
+    let li_dom = document.createElement("li");
+    li_dom.textContent = li;
+    divLi.appendChild(li_dom);
+    li_dom.addEventListener("click", clickLi());
   }
-}
-let newUstensilesLi = [...new Set(UstensilesLi)];
-for (let index = 0; index < newUstensilesLi.length; index++) {
-  const li = newUstensilesLi[index];
-  let li_dom = document.createElement("li");
-  li_dom.textContent = li;
-  UstensilesBtn.appendChild(li_dom);
 }
 
 function btnToggle(btn) {
@@ -65,17 +99,16 @@ function btnToggle(btn) {
 }
 
 //NBR DE RECETTES
-
+function nbrRecette(array){
 const nbrRecettes = document.getElementById("recetteNbr");
-if (recipes.length < 2) {
-  nbrRecettes.textContent = recipes.length + " Recette";
+if (array.length < 2) {
+  nbrRecettes.textContent = array.length + " Recette";
 } else {
-  nbrRecettes.textContent = recipes.length + " Recettes";
-}
-
-console.log(recipes);
+  nbrRecettes.textContent = array.length + " Recettes";
+}}
 
 //<li> CHANGEMENT DE STYLE AU CLICK
+function clickLi(){
 let li = document.querySelectorAll("li");
 li.forEach((element) => {
   element.addEventListener("click", () => {
@@ -83,32 +116,16 @@ li.forEach((element) => {
       element.style.backgroundColor === "" ? "#ffd15b" : "";
     element.style.margin = element.style.margin === "" ? "5px auto" : "";
   });
-});
+});}
 
-searchDOM.addEventListener("input", () => {
-  if (searchDOM.value.length > 2) {
-    let searchTerm = searchDOM.value;
-    // Utilisez la méthode filter pour créer un nouvel array avec les résultats de la recherche
-    const resultatName = recipes.filter((recip) =>
-      recip.name.toLowerCase().includes(searchTerm.toLowerCase())
-    );
-    const resultatdescription = recipes.filter((recip) =>
-      recip.description.toLowerCase().includes(searchTerm.toLowerCase())
-    );
-    
-    let resultatIngredients = [];
-      for (let index = 0; index < recipes.length; index++) {
-        const element = recipes[index].ingredients;
-        const resultatIngredient = element.filter((recip) =>
-        recip.ingredient.toLowerCase().includes(searchTerm.toLowerCase()) 
-        );
-        if (resultatIngredient.length>0) {
-            resultatIngredients.push(recipes[index])
-        }
-      }
-    
 
-    console.log(resultatName,resultatdescription);
-    console.log(resultatIngredients, "ingredient");
-    }
-});
+//Fonction qui lance l'affichage de la page
+function init(array) {
+  cardContent.innerHTML = "";
+  createLi(ingredientsBtn, array);
+  createLi(AppareilsBtn, array);
+  createLi(UstensilesBtn, array);
+  nbrRecette(array);
+  cardDisplay(array);
+}
+init([...recipes]);
